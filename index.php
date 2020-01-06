42d3068194531ebcb22371c4342df4def067b6dd
InoueAkitaka <inoue.akitaka@mari-magno.jp>
2020-01-06 19:52:28 +0900

    no message

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-	// ダウンロードするサーバのファイルパス
-	$filepath = 'sample.csv';
-
-	// HTTPヘッダを設定
-	header('Content-Type: application/octet-stream');
-	header('Content-Length: '.filesize($filepath));
-	header('Content-Disposition: attachment; filename=download.csv');
-
-	// ファイル出力
-	readfile($filepath);
-
 	echo $_GET['page'];
 ?>
 
