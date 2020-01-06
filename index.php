05cce653c1641a0cebcdbdc91737a4d637c42184
InoueAkitaka <inoue.akitaka@mari-magno.jp>
2020-01-06 20:51:13 +0900

    no message

--- index.php
+++ index.php
@@ -1,5 +1,53 @@
 <?php
+	// ダウンロードするサーバのファイルパス
+	$filepath = 'sample.csv';
+
+	// HTTPヘッダを設定
+	header('Content-Type: application/octet-stream');
+	header('Content-Length: '.filesize($filepath));
+	header('Content-Disposition: attachment; filename=download.csv');
+
+	// ファイル出力
+	readfile($filepath);
+
 	echo $_GET['page'];
+
+// linebotのDBに接続
+// 環境変数(getenv)はherokuのappに記載する必要がある
+class dbConnection {
+	// インスタンス
+	protected static $db;
+	
+	// コンストラクタ
+	private function __construct() {
+		try {
+			// 環境変数からデータベースへの接続情報を取得
+			$url = parse_url(getenv('DATABASE_URL'));
+			
+			// データソース
+			$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+			
+			// 接続を確立
+			self::$db = new PDO($dsn, $url['user'], $url['pass']);
+			
+			// エラー時、例外をスロー
+			self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $e){
+			echo 'Connection Error: ' . $e->getMessage();
+		}
+	}
+	
+	// シングルトン
+	// 存在しない場合のみインスタンス化
+	public static function getConnection() {
+		if (!self::$db) {
+			new dbConnection();
+		}
+		
+		return self::$db;
+	}
+}
 ?>
 
 <p>test</p>
