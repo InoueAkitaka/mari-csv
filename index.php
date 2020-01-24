bfbd41a6f4496db8c6cc5299351667be731a8981
InoueAkitaka <inoue.akitaka@mari-magno.jp>
2020-01-24 20:06:25 +0900

    no message

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 define('M_USER', 'm_line_user_data');
 define('T_TIME', 't_line_time_card');
 
+// 時間を切り上げる
+$nowEditTime = ceilPerTime(strtotime(date("H:i:s")), 15);
+
+echo $nowEditTime;
+
 	if ( $_POST['mode'] === 'download' ) {
 		//echo 'testtesttest';
 		
@@ -102,6 +107,65 @@ class dbConnection {
 		
 		return self::$db;
 	}
+}
+
+/**
+ * 時間(hhmm)を指定した分単位で切り上げる
+ * 
+ * @param $time 時間と分の文字列(1130, 11:30など)
+ * @param $per 切り上げる単位(分) 5分なら5
+ * @return false or 切り上げられた DateTime オブジェクト(->fomat で自由にフォーマットして使用する)
+ */
+function ceilPerTime($time, $per){
+
+    // 値がない時、単位が0の時は false を返して終了する
+    if( !isset($time) || !is_numeric($per) || ($per == 0 )) {
+        return false;
+    }else{
+        $deteObj = new DateTime($time);
+        // 指定された単位で切り上げる
+        // フォーマット文字 i だと、 例えば1分が 2桁の 01 となる(1桁は無い）ので、整数に変換してから切り上げる
+        $ceil_num = ceil(sprintf('%d', $deteObj->format('i'))/$per) *$per;
+
+        // 切り上げた「分」が60になったら「時間」を1つ繰り上げる
+        // 60分 -> 00分に直す
+        $hour = $deteObj->format('H');
+
+        if( $ceil_num == 60 ) {
+            $hour = $deteObj->modify('+1 hour')->format('H');
+            $ceil_num = '00';
+        }
+        $have = $hour.sprintf( '%02d', $ceil_num );
+
+        return new DateTime($have);
+    }
+}
+
+/**
+ * 時間(hhmm)を指定した分単位で切り捨てる
+ * 
+ * @param $time 時間と分の文字列(1130, 11:30など)
+ * @param $per 切り捨てる単位(分) 5分なら5
+ * @return false or 切り捨てられた DateTime オブジェクト(->fomat で自由にフォーマットして使用する)
+ */
+function floorPerTime($time, $per){
+
+    // 値がない時、単位が0の時は false を返して終了する
+    if( !isset($time) || !is_numeric($per) || ($per == 0 )) {
+        return false;
+    }else{
+        $deteObj = new DateTime($time);
+
+        // 指定された単位で切り捨てる
+        // フォーマット文字 i だと、 例えば1分が 2桁の 01 となる(1桁は無い）ので、整数に変換してから切り捨てる
+        $ceil_num = floor(sprintf('%d', $deteObj->format('i'))/$per) *$per;
+
+        $hour = $deteObj->format('H');
+
+        $have = $hour.sprintf( '%02d', $ceil_num );
+
+        return new DateTime($have);
+    }
 }
 ?>
 
